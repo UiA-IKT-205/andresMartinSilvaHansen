00fb6aee1b0b158b9ef823d916d1786db57804b5
Andres Hansen <amhansen@uia.no>
2021-02-23 19:10:32 +0100

    Piano App

---
+++ app/src/main/java/no/uia/ikt205/andres/pianoapp/FullTonePianoKeyFragment.kt
@@ -0,0 +1,59 @@
+package no.uia.ikt205.andres.pianoapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_full_tone_piano.view.*
+import no.uia.ikt205.andres.pianoapp.databinding.FragmentFullTonePianoBinding
+
+class FullTonePianoKeyFragment : Fragment() {
+
+    private var _binding:FragmentFullTonePianoBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var note:String
+
+    var onKeyDown:((note:String) -> Unit)? = null
+    var onKeyUp:((note:String) -> Unit)? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            note = it.getString("NOTE") ?: "?"
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFullTonePianoBinding.inflate(inflater)
+        val view = binding.root
+
+        view.fullToneKey.setOnTouchListener(object : View.OnTouchListener{
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                when(event?.action){
+                    MotionEvent.ACTION_DOWN -> this@FullTonePianoKeyFragment.onKeyDown?.invoke(note)
+                    MotionEvent.ACTION_UP -> this@FullTonePianoKeyFragment.onKeyUp?.invoke(note)
+                }
+                return true
+            }
+        })
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_full_tone_piano, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(note: String) =
+            FullTonePianoKeyFragment().apply {
+                arguments = Bundle().apply {
+                    putString("NOTE", note)
+                }
+            }
+    }
+}
